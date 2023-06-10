2ee38dd1e3fb13f722ddb6ad9f6ff17847337ad0
秦思成-MacBook <464815411@qq.com>
2023-06-10 11:52:01 +0800

    完成用户登录校验

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  get "pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "quotes#index"
+  root to: "pages#home"
   resources :quotes
 end
