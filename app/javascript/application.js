484b8a5e845a3aeda44982920225cd4ac1bea79a
秦思成-MacBook <464815411@qq.com>
2023-06-11 20:20:10 +0800

    use flash message without Turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// The two following lines disable Turbo on the whole application
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
