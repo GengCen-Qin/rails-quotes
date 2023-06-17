678659e7d7f853031132e38377e428134bb36925
秦思成-MacBook <464815411@qq.com>
2023-06-17 21:45:20 +0800

    完成第七章代码修改，将create，update，destroy操作显示flash message

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -18,7 +18,7 @@ class QuotesController < ApplicationController
     if @quote.save
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully created"}
       end
     else
       render :new, status: :unprocessable_entity
@@ -30,7 +30,10 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -38,10 +41,10 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-
+  
     respond_to do |format|
       format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
-      format.turbo_stream
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed." }
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def render_turbo_stream_flash_messages
+        turbo_stream.prepend "flash", partial: "layouts/flash"
+    end
 end
